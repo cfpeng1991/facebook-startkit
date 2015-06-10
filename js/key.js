67e0a0a30522a1957ac4ad10e71236feabbb2c16
cfpeng1991 <cfpeng1991@gmail.com>
2015-06-10 19:43:30 +0800

    g

--- js/key.js
+++ js/key.js
@@ -1,6 +1,18 @@
-/*請貼上fackbook初始化的相關程式碼*/
-/*https://developers.facebook.com/*/
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1019738954711387',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
 
-
-
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
